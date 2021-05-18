5ed34a39d456972f5f53ee3797287ea50c1d5f19
Vivek Kumar Gurung <z3ro647@gmail.com>
2021-05-18 19:58:33 +0545

    Activity load animaiton removed and Blank Simple Quiz Menu Added

---
+++ app/src/main/java/com/example/myprogrammingquizapp/SimpleQuizMenu.java
@@ -0,0 +1,13 @@
+package com.example.myprogrammingquizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleQuizMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_quiz_menu);
+    }
+}
