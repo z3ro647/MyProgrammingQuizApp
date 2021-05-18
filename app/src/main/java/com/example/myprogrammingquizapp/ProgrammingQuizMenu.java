7460aaa76973bfe2389c74fb23f49350fc67cb79
Vivek Kumar Gurung <z3ro647@gmail.com>
2021-05-18 21:00:12 +0545

    Blank Programming Quiz Menu Added

---
+++ app/src/main/java/com/example/myprogrammingquizapp/ProgrammingQuizMenu.java
@@ -0,0 +1,13 @@
+package com.example.myprogrammingquizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgrammingQuizMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programming_quiz_menu);
+    }
+}
